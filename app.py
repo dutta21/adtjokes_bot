65ab61b4d913ab88242e5e9cd8267518c0551e96
Aritra <duttaaritra30@gmail.com>
2026-04-12 15:16:57 +0530

    looping it and storing it

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import requests
+import time
 
 url = "https://v2.jokeapi.dev/joke/Dark"
-response = requests.get(url)
-data = response.json()
-df = pd.json_normalize(data)
-print(df)
+
+output_file = "check.xlsx"
+dfx=pd.DataFrame()
+for i in range(6):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.json_normalize(data)
+        dfx=pd.concat([df,dfx],ignore_index=True)
+    else:
+        print("Error:",response.status_code)
+    
+    time.sleep(10)
+
+dfx.to_excel(output_file,header=True,index=False)
